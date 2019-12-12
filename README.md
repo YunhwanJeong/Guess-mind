be8c07555c5267d9633a88e21c2100b5268796ba
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-12-12 14:49:52 +0900

    Finish up to server setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Guess-Mind
 
 Realtime Drawing Game built with Socket.IO, Gulp and Node.js
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [x] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
 import express from "express";
+import { join } from "path";
+import socketIO from "socket.io";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+console.log(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleListening = () =>
+  console.log(`Server is running at http://localhost:${PORT}✅`);
+
+app.listen(PORT, handleListening);
