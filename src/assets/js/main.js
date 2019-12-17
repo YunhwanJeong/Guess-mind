4425880f1174b98a599f23eb2a37d09be5994ea9
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-12-17 16:54:40 +0900

    Finish up to display whether loggedIn or loggedOut

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,27 @@
-import "./chat";
+const body = document.querySelector("body");
+const loginBox = document.getElementById("jsLoginBox");
+const loginForm = loginBox.querySelector("form");
+const loginInput = loginBox.querySelector("input");
+const gameContainer = document.getElementById("jsGameContainer");
+
+const LOGGED_IN = "loggedIn";
+const LOGGED_OUT = "loggedOut";
+const NICKNAME = "nickname";
+const nickname = localStorage.getItem(NICKNAME);
+
+const handleSubmitNickname = event => {
+  event.preventDefault();
+  const nickname = loginInput.value;
+  localStorage.setItem(NICKNAME, nickname);
+  loginInput.value = "";
+};
+
+if (nickname === null) {
+  body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
+}
+
+if (loginBox) {
+  loginForm.addEventListener("submit", handleSubmitNickname);
+}
